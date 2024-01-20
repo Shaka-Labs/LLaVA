330162da3bb60f1b03ac30537f5985de279a6b9c
Antonio Arbues <avarbues@gmail.com>
2024-01-20 18:47:48 +0100

    lora taxi finetuning script

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -3,9 +3,9 @@
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path liuhaotian/llava-v1.5-13b \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
+    --data_path ./playground/data/dataset_taxi.json \
     --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
@@ -15,14 +15,14 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora \
+    --output_dir ./checkpoints/llava-v1.5-7b-taxi-lora \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 128 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 4998 \
     --save_total_limit 1 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
