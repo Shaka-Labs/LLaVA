608ab1fc7906a3b77240dc1b2557d961e0c8effd
Antonio Arbues <avarbues@gmail.com>
2024-01-22 12:12:16 +0100

    cli taxi closed loop script

---
+++ llava/serve/cli_taxi_cl.py
@@ -0,0 +1,146 @@
+import argparse
+import torch
+
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import process_images, tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+
+from PIL import Image
+
+import requests
+from PIL import Image
+from io import BytesIO
+from transformers import TextStreamer
+
+import gymnasium as gym
+from tqdm import tqdm
+
+
+def load_image(image_file):
+    if image_file.startswith('http://') or image_file.startswith('https://'):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
+    return image
+
+
+def reset_conv(conv):
+    conv.messages = ()
+    return conv
+
+def main(args):
+    # Model
+    disable_torch_init()
+
+    model_name = get_model_name_from_path(args.model_path)
+    tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, args.model_base, model_name, args.load_8bit, args.load_4bit, device=args.device)
+
+    if 'llama-2' in model_name.lower():
+        conv_mode = "llava_llama_2"
+    elif "v1" in model_name.lower():
+        conv_mode = "llava_v1"
+    elif "mpt" in model_name.lower():
+        conv_mode = "mpt"
+    else:
+        conv_mode = "llava_v0"
+
+    if args.conv_mode is not None and conv_mode != args.conv_mode:
+        print('[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}'.format(conv_mode, args.conv_mode, args.conv_mode))
+    else:
+        args.conv_mode = conv_mode
+
+    def get_vla_action(jpg, info):
+        conv = conv_templates[args.conv_mode].copy()
+
+        image_tensor = process_images([jpg], image_processor, model.config)
+        if type(image_tensor) is list:
+            image_tensor = [image.to(model.device, dtype=torch.float16) for image in image_tensor]
+        else:
+            image_tensor = image_tensor.to(model.device, dtype=torch.float16)
+
+        if int(info["passenger_location"]) == 4:
+            task_instruction = "drop off the passenger"
+        else:
+            task_instruction = "pick up the passenger"
+        inp = f"Q: what action should the taxi take to {task_instruction}? \n{DEFAULT_IMAGE_TOKEN} A:"
+        conv.append_message(conv.roles[0], inp)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+        streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=image_tensor,
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                max_new_tokens=args.max_new_tokens,
+                streamer=streamer,
+                use_cache=True,
+                stopping_criteria=[stopping_criteria])
+
+        outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+        # conv.messages[-1][-1] = outputs
+        return outputs
+
+    def run_taxi_vla_closed_loop():
+        def extract_passenger_location(state):
+            state_without_destination = state // 4
+            passenger_location = state_without_destination % 5
+            return passenger_location
+
+        env = gym.make('Taxi-v3', render_mode="rgb_array")
+        obs, info = env.reset()
+        info = {}
+        action = None
+
+        n_done = 0
+        n_truncated = 0
+
+        NUM_STEPS = 10_000
+        for i in range(NUM_STEPS):
+            rgb = env.render()
+            passenger_location = extract_passenger_location(obs)
+            info["passenger_location"] = passenger_location
+            jpg = Image.fromarray(rgb).convert('RGB')
+            action = get_vla_action(jpg, info)
+            print(f"str {action=}")
+            action = int(action)
+            print(f"int {action=}")
+            obs, reward, done, truncated, info = env.step(action)
+
+            n_done += done
+            n_truncated += truncated
+
+            print(f"{i=}, {done=}, {truncated=}")
+
+            if done:
+                obs, info = env.reset()
+        
+        print(f"{n_done=}, {n_truncated=}")
+
+    run_taxi_vla_closed_loop()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
+    parser.add_argument("--model-base", type=str, default=None)
+    parser.add_argument("--image-file", type=str, required=True)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--conv-mode", type=str, default=None)
+    parser.add_argument("--temperature", type=float, default=0.2)
+    parser.add_argument("--max-new-tokens", type=int, default=512)
+    parser.add_argument("--load-8bit", action="store_true")
+    parser.add_argument("--load-4bit", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    main(args)
